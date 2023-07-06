83db81ad3338ffc1c4be26188f59fb5ad21616be
jacobfishel <138823440+jacobfishel@users.noreply.github.com>
2023-07-06 21:25:29 +0000

    Finished program price

---
+++ hello.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+
+int main() {
+double price;
+cout << "Enter meal price\n";
+cin >> price;
+cout << "Your meal price is: " << price * 1.08 << endl;
+double tip;
+double number;
+cout << "How much would you like to tip? (enter number only) \n10%, 15%, 20%\n";
+cin >> tip;
+if (tip == 10) {
+  price *= 1.10;
+}
+else if (tip == 15) {
+price *= 1.15;
+
+}
+else if (tip == 20) {
+price *= 1.2;
+}
+
+cout << "Your final price is: " << price << endl;
+string answer;
+cout << "Would you like to round up?\n";
+cin >> answer;
+if (answer == "yes") {
+    cout << ceil(price) << endl;
+}
+else if (answer == "no") {
+cout << price << endl;
+
+}
+
+
+
+return 0;
+}
